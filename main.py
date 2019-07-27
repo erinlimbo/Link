53cdbc0f73295e32e7bc5913301ff2eadaa01bee
Ashlee Kupor <aekupor@gmail.com>
2019-07-26 17:05:48 -0700

    added User model

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import webapp2
 import logging
 import jinja2
 import os
+from google.appengine.ext import ndb
+from google.appengine.api import users
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
+class User(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    dates_free = ndb.StringProperty(repeated=True)
+    friends = ndb.KeyProperty(repeated=True, kind=User)
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Link")
+
         template_vars = {
-        
+
         }
 
         template = jinja_env.get_template('templates/main.html')
