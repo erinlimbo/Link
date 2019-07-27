b1d21d0122e889ff4088da361bf232f587e6093d
Ashlee Kupor <aekupor@gmail.com>
2019-07-26 17:22:56 -0700

    Added new templates and handlers for different pages.

--- main.py
+++ main.py
@@ -12,19 +12,62 @@ jinja_env = jinja2.Environment(
 class User(ndb.Model):
     name = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
-    dates_free = ndb.StringProperty(repeated=True)
-    friends = ndb.KeyProperty(repeated=True, kind=User)
+    dates_free = ndb.DateProperty(repeated=True)
+    # friends = ndb.KeyProperty(repeated=True, kind=User) --> this is a many to many relationship
 
-class MainPage(webapp2.RequestHandler):
+class Login(webapp2.RequestHandler):
     def get(self):
-
         template_vars = {
 
         }
+        template = jinja_env.get_template('templates/login.html')
+        self.response.write(template.render(template_vars))
 
-        template = jinja_env.get_template('templates/main.html')
+class Home(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/home.html')
+        self.response.write(template.render(template_vars))
+
+class Profile(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/profile.html')
+        self.response.write(template.render(template_vars))
+
+class Friends(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/friends.html')
+        self.response.write(template.render(template_vars))
+
+class Schedule(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/schedule.html')
+        self.response.write(template.render(template_vars))
+
+class Linkup(webapp2.RequestHandler):
+    def get(self):
+        template_vars = {
+
+        }
+        template = jinja_env.get_template('templates/linkup.html')
         self.response.write(template.render(template_vars))
 
 app=webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Login),
+    ('/home', Home),
+    ('/profile', Profile),
+    ('/friends', Friends),
+    ('/schedule', Schedule),
+    ('/linkup', Linkup),
 ], debug=True)
