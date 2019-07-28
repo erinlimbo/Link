6430dbea273855efbf71f254cca7dd8542453c00
alexarmez <alexa.ramirez0480@gmail.com>
2019-07-28 16:50:28 -0700

    Added emails user

--- main.py
+++ main.py
@@ -33,9 +33,13 @@ class Home(webapp2.RequestHandler):
 
 class Profile(webapp2.RequestHandler):
     def get(self):
+        current_user = users.get_current_user()
+        # first_name = users.name.familyName()
         template_vars = {
-
+            'current_user': current_user,
+            # 'first_name' : first_name
         }
+
         template = jinja_env.get_template('templates/profile.html')
         self.response.write(template.render(template_vars))
 
